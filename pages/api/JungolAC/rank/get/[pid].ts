1b342dc4d256f6c59d815f5393d252cb640c6ef2
Oein <oein0219@gmail.com>
2022-06-26 18:11:30 +0900

    Fix error

--- pages/api/JungolAC/rank/get/[pid].ts
+++ pages/api/JungolAC/rank/get/[pid].ts
@@ -13,26 +13,30 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  let { pid } = req.query;
-  pid = pid.toString();
-  if (ranks[pid] == undefined) {
-    res.end("No problem found");
+  try {
+    let { pid } = req.query;
+    pid = pid.toString();
+    if (ranks[pid] == undefined) {
+      res.end("No problem found");
+    }
+    const dbKey = ranks[pid] + " # " + pid;
+    const data = await prisma.problemVote.findFirst({
+      where: {
+        name: dbKey,
+      },
+    });
+    const votes = data?.ranks as { [key: number]: number };
+    let allC = 0;
+    let cont = 0;
+    for (let i = 0; i <= 30; i++) {
+      allC += votes[i] * i;
+      cont += votes[i];
+    }
+    if (cont == 0) cont = 1;
+    allC /= cont;
+    allC = Math.round(allC);
+    res.status(200).json({ ProblemRank: allC });
+  } catch {
+    res.status(200).json({ ProblemRank: 0 });
   }
-  const dbKey = ranks[pid] + " # " + pid;
-  const data = await prisma.problemVote.findFirst({
-    where: {
-      name: dbKey,
-    },
-  });
-  const votes = data?.ranks as { [key: number]: number };
-  let allC = 0;
-  let cont = 0;
-  for (let i = 0; i <= 30; i++) {
-    allC += votes[i] * i;
-    cont += votes[i];
-  }
-  if (cont == 0) cont = 1;
-  allC /= cont;
-  allC = Math.round(allC);
-  res.status(200).json({ ProblemRank: allC });
 }
