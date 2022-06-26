a1d2e22c64cb4a9b2c95c3c9c5527124af9001f8
Oein <oein0219@gmail.com>
2022-06-26 11:34:34 +0900

    Add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import FHeader from "../components/Fheader";
 
 export default function Index() {
   return (
     <>
+      <Head>
+        <title>Jungoler | Main</title>
+      </Head>
       <FHeader
         title="Jungoler"
         description="Jungol.co.kr을 사용하는 사람들의 모임"
